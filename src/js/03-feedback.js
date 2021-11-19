663bbcd5e321b818311b39dd0f85ed7befb63bbe
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2021-11-19 19:29:28 +0200

    11

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+// Инициализация библтотеки
+import { throttle } from 'lodash';
 
+// Инициализация узлов
+const feedBackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const textArea = document.querySelector('.feedback-form textarea');
+const btn = document.querySelector('.feedback-form button');
+
+// ----------------------------------
+// ----------- Оюработчик -----------
+// ----------------------------------
+const onEmailInput = () => {
+  console.log('Yesssssssssssssss');
+};
+
+const onTextAreaInput = event => {
+  console.log(event);
+  console.log(textArea.value);
+};
+
+const onBtnPress = event => {
+  console.log({ event });
+};
+
+// ----------------------------------
+// ----------- Слушатели ------------
+// ----------------------------------
+email.addEventListener('input', throttle(onEmailInput, 500));
+textArea.addEventListener('input', throttle(onTextAreaInput, 500));
+btn.addEventListener('click', throttle(onBtnPress, 500));
+// btn.addEventListener('submit', throttle(onBtn, 500));
